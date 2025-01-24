3c21a8e114282fa76783a28aa068191899e999b7
Marek Algoud <marek.algoud@gmail.com>
2025-01-24 19:46:20 +0100

    feat: ajout du footer et correction icone du manifest.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component, inject } from '@angular/core'
 import { StatsService } from './services/stats.service'
 import {RouterOutlet} from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, FooterComponent],
 })
 export class AppComponent {
 
